b766a1cc350f51acca4a4df7908a10f5e3300676
damonmaldonado <damon@mpwmarketing.com>
2016-03-11 14:50:37 -0500

    Create embed-wrap.js

---
+++ js/embed-wrap.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($){
+
+    $('iframe[src*="youtube.com"]').each(function() {
+    	if ( $(this).parent().hasClass('embed-container') || $(this).hasClass('wp-theater-iframe') || $(this).hasClass('') === false ) {
+    		return;
+    	} else {
+    		$(this).wrap('<div class="embed-container"></div>');
+    	}
+    }); 
+
+});
